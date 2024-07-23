42fed5aa615753543c1dee168c4910fb921de27f
Julia Domingues <142114663+julooch@users.noreply.github.com>
2024-07-23 14:31:57 -0300

    feat: initial config

---
+++ src/main/java/com/projeto/geladas/GeladasApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.geladas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeladasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeladasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/geladas/GeladasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.geladas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeladasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
